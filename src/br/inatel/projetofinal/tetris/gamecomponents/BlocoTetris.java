4e0f87b166c8bbed4e250edbaa94082796d0e482
Luiz Filipe <luiz.lopes@get.inatel.br>
2023-05-05 00:08:02 -0300

    Mais getters e um método para fazer o spawn dos blocos acima do grid

--- src/br/inatel/projetofinal/tetris/gamecomponents/BlocoTetris.java
+++ src/br/inatel/projetofinal/tetris/gamecomponents/BlocoTetris.java
@@ -5,10 +5,16 @@ import java.awt.Color;
 public class BlocoTetris {
     private int[][] forma;
     private Color cor;
+    private int x, y;
     
     public BlocoTetris(int[][] forma, Color cor) {
         this.forma = forma;
         this.cor = cor;
+    }
+    
+    public void spawn(int TamanhoGrid) {
+        y = -getAltura();
+        x = (TamanhoGrid -getLargura()) / 2;
     }
 
     public int[][] getForma() {
@@ -26,4 +32,30 @@ public class BlocoTetris {
     public int getLargura() {
         return forma[0].length;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+    //Metodos de controle do bloco
+    public void moverParaBaixo() {
+        y++;
+    }
+    
+    public void moverLadoDireito() {
+        x++;
+    }
+    
+    public void moverLadoEsquerdo() {
+        x--;
+    }
+    
+    public int inferior() {
+        return y + getAltura();
+    }
+    
 }
